29420b49d81e718b57bd658369784413871c2196
shreyajain9589 <shreyajain8739@gmail.com>
2025-11-30 17:44:04 +0530

    remove webcontainer in production

--- src/config/webContainer.js
+++ src/config/webContainer.js
@@ -2,10 +2,14 @@ import { WebContainer } from '@webcontainer/api';
 
 let webContainerInstance = null;
 
-
 export const getWebContainer = async () => {
-    if (webContainerInstance === null) {
-        webContainerInstance = await WebContainer.boot();
-    }
-    return webContainerInstance;
-}
+  if (import.meta.env.PROD) {
+    console.warn("WebContainer is disabled in production");
+    return null;
+  }
+
+  if (webContainerInstance === null) {
+    webContainerInstance = await WebContainer.boot();
+  }
+  return webContainerInstance;
+};
